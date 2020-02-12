d47749887371076b1b7c1e152b0d36224b2231f4
Dominik Michalik <dominik.michalik@okfelix.com>
2020-02-12 15:39:55 +0100

    add: controls to ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends React.Component {
 
@@ -16,7 +17,7 @@ class BurgerBuilder extends React.Component {
         return (
           <>
               <Burger ingredients={this.state.ingredients} />
-              <div>Build controls</div>
+              <BuildControls />
           </>
         );
     }

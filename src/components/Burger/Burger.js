e4bdf2158036c0683952da75f0c408c282c4d4a6
Dominik Michalik <dominik.michalik@okfelix.com>
2020-02-12 15:21:18 +0100

    maintain: handle empty ingredients array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = ({ ingredients }) => {
-    const transformedIngredients = Object.keys(ingredients)
+    let transformedIngredients = Object.keys(ingredients)
         .map((igKey) => {
             return [...Array(ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Add some ingredients!</p>
+    }
 
     return (
         <div className={classes["Burger"]}>

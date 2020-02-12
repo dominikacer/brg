23b25349ed6fae47145007523c6737a19b57bfeb
Dominik Michalik <dominik.michalik@okfelix.com>
2020-02-12 13:51:17 +0100

    add: prepare Layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Layout = ({ children }) => (
+  <>
+    <div>
+      Toolbar, SideDrawer, Backdrop
+    </div>
+    <main>
+        {children}
+    </main>
+  </>
+);
+
+export default Layout;
